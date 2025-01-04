b84bd0b59bfd8351c6edac9254b9a97b612f6c2e
sourabh-yalagod <sourabhofficial99804@gmail.com>
2025-01-04 20:36:45 +0530

    bug fixed

--- client/src/lib/SecureRouters.jsx
+++ client/src/lib/SecureRouters.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { jwtDecode } from "jwt-decode";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 const SecureRouters = () => {
-    const token = localStorage.getItem("token");
-  //   const decoded = jwtDecode(token);
-  //   const userId = decoded?.id;
-  const userId = true;
-
+  const token = localStorage.getItem("token");
   if (token) {
     return <Outlet />;
   } else {
-    return <Navigate to={'/signin'} />
+    return <Navigate to={"/signin"} />;
   }
 };
 
